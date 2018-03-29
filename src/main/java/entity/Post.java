c8ee98f391311cdabb4d644e6ab5667c1af76be0
bohdanrud <bohdanrud5180@gmail.com>
2018-03-29 17:37:36 +0300

    Lesson 8

--- src/main/java/entity/Post.java
+++ src/main/java/entity/Post.java
@@ -3,14 +3,17 @@ package entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import entity.enums.Status;
@@ -36,6 +39,10 @@ public class Post extends BaseEntity{
 	
 	@OneToMany(mappedBy = "post")
 	private List<Comment> comments = new ArrayList<Comment>();
+	
+	@OneToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id")
+	private Product product;
 	
 	@ManyToMany
 	@JoinTable(name = "post_tag", 
--- src/main/java/ua/logos/orm/Lesson_6_7/App.java
+++ src/main/java/ua/logos/orm/Lesson_6_7/App.java
@@ -1,5 +1,6 @@
 package ua.logos.orm.Lesson_6_7;
 
+import java.math.BigDecimal;
 import java.util.*;
 import java.util.List;
 
@@ -9,116 +10,147 @@ import javax.persistence.Persistence;
 
 import entity.Comment;
 import entity.Post;
+import entity.Product;
 import entity.Tag;
 import entity.enums.Status;
 
+public class App {
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("mysql");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-       EntityManagerFactory factory = Persistence.createEntityManagerFactory("mysql");
-       EntityManager em = factory.createEntityManager();
-       em.getTransaction().begin();
-       
-//       addTags(em);
-//       addPost(em);
-//       addComment(em);
-       
-       List<Comment> comments = em.createQuery("SELECT c FROM Comment c", Comment.class).getResultList();    //all from comment`s table
-       
-       comments.forEach(c -> System.out.println(c));
-       
-       Comment commentById =  em.createQuery("SELECT c FROM Comment c WHERE c.id = :id", Comment.class).setParameter("id", 40).getSingleResult();   //return comment with id = 40;
-       System.out.println(commentById);
-       
-       List<Post> posts = em.createQuery("SELECT p FROM Post p WHERE p.id > :post_id", Post.class).setParameter("post_id", 50).getResultList();
-       posts.forEach(p -> System.out.println(p));
-       
-       List<Post> posts1 = em.createQuery("SELECT p FROM Post p WHERE p.id IN (:ids)", Post.class).setParameter("ids", Arrays.asList(2,56,3,76,99,34)).getResultList();
-       posts1.forEach(p -> System.out.println(p));
-       
-       List<Post> posts2 = em.createQuery("SELECT p FROM Post p WHERE p.title LIKE :post_title", Post.class).setParameter("post_title", "%8").getResultList();   //8 on the end
-       posts2.forEach(p -> System.out.println(p));
-       
-       List<Post> posts3 = em.createQuery("SELECT p FROM Post p WHERE p.title LIKE :post_title", Post.class).setParameter("post_title", "%8_").getResultList();   //8 on the end
-       posts3.forEach(p -> System.out.println(p));
-       
-       List<Post> posts4 = em.createQuery("SELECT p FROM Post p WHERE p.id BETWEEN :first AND :last", Post.class).setParameter("first",76).setParameter("last", 82).getResultList();
-       posts4.forEach(p -> System.out.println(p));
-       
-       //Agreg functoin
-       
-       Long count = em.createQuery("SELECT count(c.id) FROM Comment c", Long.class).getSingleResult();
-       System.out.println("Count: " + count);
-       
-       Long sum = em.createQuery("SELECT sum(c.id) FROM Comment c", Long.class).getSingleResult();
-       System.out.println("Sum: " + sum);
-       
-       Double average = em.createQuery("SELECT avg(c.id) FROM Comment c", Double.class).getSingleResult();
-       System.out.println("Avarege: " + average);
-       
-       Integer max = em.createQuery("SELECT max(c.id) FROM Comment c", Integer.class).getSingleResult();
-       System.out.println("MAX: " + max);
-       
-       Integer min = em.createQuery("SELECT min(c.id) FROM Comment c", Integer.class).getSingleResult();
-       System.out.println("MIN: " + min);
-       
-       em.getTransaction().commit();
-       em.close();
-       factory.close();
-       
-       
-       
-    }
-    
-    private static void addTags(EntityManager em) {
-    	List<String> tags = new ArrayList<String>();
-    	tags.add("Java");
-    	tags.add("SQL");
-    	tags.add("ORM");
-    	tags.add("JPL");
-    	tags.add("MySQL");
-    	tags.add("STS");
-    	tags.add("Eclipse");
-    	
-    	for (int i = 0; i < tags.size(); i++) {
+		 //addTags(em);
+		 //addPost(em);
+		// addComment(em);
+
+		// List<Comment> comments = em.createQuery("SELECT c FROM Comment c",
+		// Comment.class).getResultList(); //all from comment`s table
+		// comments.forEach(c -> System.out.println(c));
+		//
+		// Comment commentById = em.createQuery("SELECT c FROM Comment c WHERE c.id =
+		// :id", Comment.class).setParameter("id", 40).getSingleResult(); //return
+		// comment with id = 40;
+		// System.out.println(commentById);
+		//
+		// List<Post> posts = em.createQuery("SELECT p FROM Post p WHERE p.id >
+		// :post_id", Post.class).setParameter("post_id", 50).getResultList();
+		// posts.forEach(p -> System.out.println(p));
+		//
+		// List<Post> posts1 = em.createQuery("SELECT p FROM Post p WHERE p.id IN
+		// (:ids)", Post.class).setParameter("ids",
+		// Arrays.asList(2,56,3,76,99,34)).getResultList();
+		// posts1.forEach(p -> System.out.println(p));
+		//
+		// List<Post> posts2 = em.createQuery("SELECT p FROM Post p WHERE p.title LIKE
+		// :post_title", Post.class).setParameter("post_title", "%8").getResultList();
+		// //8 on the end
+		// posts2.forEach(p -> System.out.println(p));
+		//
+		// List<Post> posts3 = em.createQuery("SELECT p FROM Post p WHERE p.title LIKE
+		// :post_title", Post.class).setParameter("post_title", "%8_").getResultList();
+		// //8 on the end
+		// posts3.forEach(p -> System.out.println(p));
+		//
+		// List<Post> posts4 = em.createQuery("SELECT p FROM Post p WHERE p.id BETWEEN
+		// :first AND :last", Post.class).setParameter("first",76).setParameter("last",
+		// 82).getResultList();
+		// posts4.forEach(p -> System.out.println(p));
+
+		// Agreg functoin
+
+		// Long count = em.createQuery("SELECT count(c.id) FROM Comment c",
+		// Long.class).getSingleResult();
+		// System.out.println("Count: " + count);
+		//
+		// Long sum = em.createQuery("SELECT sum(c.id) FROM Comment c",
+		// Long.class).getSingleResult();
+		// System.out.println("Sum: " + sum);
+		//
+		// Double average = em.createQuery("SELECT avg(c.id) FROM Comment c",
+		// Double.class).getSingleResult();
+		// System.out.println("Avarege: " + average);
+		//
+		// Integer max = em.createQuery("SELECT max(c.id) FROM Comment c",
+		// Integer.class).getSingleResult();
+		// System.out.println("MAX: " + max);
+		//
+		// Integer min = em.createQuery("SELECT min(c.id) FROM Comment c",
+		// Integer.class).getSingleResult();
+		// System.out.println("MIN: " + min);
+		
+//		Product product = em.createQuery("SELECT p FROM Product p WHERE p.id = ?1", Product.class).setParameter(1, 5).getSingleResult();
+//		System.out.println(product);
+//
+//		Post post = em.createQuery("SELECT p FROM Post p WHERE p.id = ?1", Post.class).setParameter(1, 5).getSingleResult();
+//		System.out.println(post);
+		
+		Post post = em.createQuery("SELECT p FROM Post p RIGHT JOIN FETCH p.product pp WHERE p.id = :id", Post.class).setParameter("id", 9).getSingleResult();
+		System.out.println(post);
+		System.out.println(post.getProduct());
+		
+		em.getTransaction().commit();
+		em.close();
+		factory.close();
+
+	}
+
+	private static void addTags(EntityManager em) {
+		List<String> tags = new ArrayList<String>();
+		tags.add("Java");
+		tags.add("SQL");
+		tags.add("ORM");
+		tags.add("JPL");
+		tags.add("MySQL");
+		tags.add("STS");
+		tags.add("Eclipse");
+
+		for (int i = 0; i < tags.size(); i++) {
 			Tag tag = new Tag();
 			tag.setName(tags.get(i));
 			em.persist(tag);
 		}
-    }
-    
-    private static void addPost(EntityManager em) {
-    	
-    	for (int i = 1; i <= 100; i++) {
+	}
+
+	private static void addPost(EntityManager em) {
+
+		for (int i = 1; i <= 100; i++) {
 			Post post = new Post();
 			post.setTitle("Post title" + i);
 			post.setContent("Long post content#" + i);
-			if(i%2==0) post.setStatus(Status.DRAFT);
-			if(i%2==1) post.setStatus(Status.PUBLISH);
+			if (i % 2 == 0)
+				post.setStatus(Status.DRAFT);
+			if (i % 2 == 1)
+				post.setStatus(Status.PUBLISH);
+
+			Product product = new Product();
+			product.setName("Product name#" + i);
+			product.setDescription("Product description#" + i);
+			product.setPrice(new BigDecimal(i + 10 + ".00"));
+			product.setInStock(15 + i);
+
+			post.setProduct(product);
+
 			em.persist(post);
-			
+
 			List<Tag> tags = em.createQuery("SELECT t FROM Tag t", Tag.class).getResultList();
 			post.setTags(tags);
 		}
-    }
-    
-    private static void addComment(EntityManager em) {
-    	
-    	for (int i = 1; i <= 100; i++) {
-			Post post = em.createQuery("SELECT p FROM Post p WHERE p.id = :id", Post.class).setParameter("id", i).getSingleResult();
-			
+	}
+
+	private static void addComment(EntityManager em) {
+
+		for (int i = 1; i <= 100; i++) {
+			Post post = em.createQuery("SELECT p FROM Post p WHERE p.id = :id", Post.class).setParameter("id", i)
+					.getSingleResult();
+
 			Comment comment = new Comment();
-			comment.setAuthor("Author #"+ i);
+			comment.setAuthor("Author #" + i);
 			comment.setComment("The best comment#" + i);
 			comment.setPost(post);
 			em.persist(comment);
-			
-			
-			
-		}
-    }
 
+		}
+	}
 
 }
